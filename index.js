45dbf16bff5a0c023f1ffd7120017bd68e5fd385
Reflex <yosfyosfgaber@gmail.com>
2026-01-03 13:57:18 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+﻿// JavaScript source code
+
+const display = document.getElementById("display");
+let expression = "";
+function appendToDisplay(input) {
+    display.value += input;
+    if (input === "x") {
+        expression += "*";
+    } else if (input === "÷") {
+        expression += "/";
+    }
+    else if (input === "^") {
+        expression += "**";
+    }
+    else {
+        expression += input;
+    }
+}
+
+function clearDisplay() {
+    display.value = "";
+    expression = "";
+}
+function deleteValue() {
+    display.value = display.value.slice(0, -1);
+
+    expression = expression.slice(0, -1);
+}
+
+function calculate() {
+     try {
+        const result = eval(expression);
+        display.value = result;
+        expression = result.toString(); // allow continuing calculation
+    } catch (err) {
+        display.value = "Error";
+        expression = "";
+    }
+
+}
+
